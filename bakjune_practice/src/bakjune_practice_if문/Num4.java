ed69a3714e413f3e61f98b1bb458621d22592f33
김동현 <rlaehdgus3543@naver.com>
2021-10-18 21:07:49 +0900

    20211018백준

---
+++ bakjune_practice/src/bakjune_practice_if문/Num4.java
@@ -0,0 +1,20 @@
+package bakjune_practice_if¹®;
+
+import java.util.Scanner;
+
+public class Num4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = Integer.parseInt(sc.nextLine());
+		int Y = Integer.parseInt(sc.nextLine());
+		if(X>0&&Y>0) {
+			System.out.println("1");
+		}else if(X>0&&Y<0) {
+			System.out.println("4");
+		}else if(X<0&&Y<0) {
+			System.out.println("3");
+		}else
+			System.out.println("2");
+	}
+}
---
+++ bakjune_practice/src/bakjune_practice_if문/Num5내일해야됨.java
@@ -0,0 +1,21 @@
+package bakjune_practice_if¹®;
+//package bakjune_practice_if¹®;
+//
+//import java.util.Scanner;
+//
+//public class Num5 {
+//
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int H = Integer.parseInt(sc.nextLine());
+//		int M = Integer.parseInt(sc.nextLine());
+//		if(X>0&&Y>0) {
+//			System.out.println("1");
+//		}else if(X>0&&Y<0) {
+//			System.out.println("4");
+//		}else if(X<0&&Y<0) {
+//			System.out.println("3");
+//		}else
+//			System.out.println("2");
+//	}
+//}
