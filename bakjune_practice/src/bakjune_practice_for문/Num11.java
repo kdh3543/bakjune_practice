337b32b9aae18881a91c2a9d0e4313e7bdf6fe9a
김동현 <rlaehdgus3543@naver.com>
2021-10-20 21:22:59 +0900

    20211020

---
+++ bakjune_practice/src/bakjune_practice_for문/Num11.java
@@ -0,0 +1,17 @@
+package bakjune_practice_for¹®;
+
+import java.util.Scanner;
+
+public class Num11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		int N = Integer.parseInt(sc.nextLine());
+		int X = Integer.parseInt(sc.nextLine());
+		for(int i = 0; i<N;i++) {
+			for(int j = N;j<i+1;j++) {
+			System.out.print(" ");
+			}
+			System.out.print("*");
+		}		
+	}
+}
