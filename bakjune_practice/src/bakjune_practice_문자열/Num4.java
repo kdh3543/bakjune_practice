797046fd26aee9e860cde34a863ed81b518106ca
김동현 <rlaehdgus3543@naver.com>
2021-11-05 00:08:35 +0900

    20211104

---
+++ bakjune_practice/src/bakjune_practice_문자열/Num4.java
@@ -0,0 +1,19 @@
+package bakjune_practice_¹®ÀÚ¿­;
+
+import java.util.Scanner;
+
+public class Num4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int[] R = new int[T];
+		for(int i = 0; i<R.length;i++) {
+			R[i]=sc.nextInt();
+			String S = sc.nextLine();
+			for(int j = 0; j<R[i];j++) {
+				
+			}
+		}
+	}	
+}
---
+++ bakjune_practice/src/bakjune_practice_함수/num1.java
@@ -0,0 +1,11 @@
+package bakjune_practice_ÇÔ¼ö;
+
+public class num1 {
+	long sum(int[] a) {
+		long ans = 0;
+		for(int i = 0; i<a.length;i++) {
+			ans += a[i];
+		}
+		return ans;
+	}
+}
