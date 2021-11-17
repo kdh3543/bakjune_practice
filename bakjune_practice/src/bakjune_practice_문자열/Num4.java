35f2177ab118bb80abca76dfddef241d45306872
김동현 <rlaehdgus3543@naver.com>
2021-11-18 00:24:13 +0900

    20211118

--- bakjune_practice/src/bakjune_practice_문자열/Num4.java
+++ bakjune_practice/src/bakjune_practice_문자열/Num4.java
@@ -6,17 +6,22 @@ public class Num4 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		int T = Integer.parseInt(sc.nextLine());
 		int[] R = new int[T];
+		
 		for(int i = 0; i<R.length;i++) {
-			R[i]=sc.nextInt();
-			sc.nextLine();
-			String S = sc.next();
-			for(int j = 0; j<R[i];j++) {
-				for(int k = 0; k<R[i];k++) {
-					System.out.print(S.charAt(j));
+			R[i]=sc.nextInt();//R[0]=3,R[1]=5
+			
+			String S=sc.nextLine();
+			
+			String[] arr = S.split("");
+			
+			for(int j = 1; j<arr.length;j++) {
+				for(int k = 0; k<R[i];k++) {					
+					System.out.print(arr[j]);
 				}
 			}
+			System.out.println();
 		}
 	}	
 }
