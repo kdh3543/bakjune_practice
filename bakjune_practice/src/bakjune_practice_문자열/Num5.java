e74714aae19ad49560567ca7ffadce0835e36bf5
김동현 <rlaehdgus3543@naver.com>
2021-11-22 00:03:43 +0900

    20211122

--- bakjune_practice/src/bakjune_practice_문자열/Num5.java
+++ bakjune_practice/src/bakjune_practice_문자열/Num5.java
@@ -8,14 +8,13 @@ public class Num5 {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		int[] alpha = new int[26];
-		char[] arr = new char[str.length()];
 		int[] count = new int[26];
-		
+
 		for(int i = 0; i<alpha.length;i++) {
 			alpha[i] = i;
-			count[i] = 1;
+			count[i] = 0;
 		}
-		
+
 		for(int i = 0; i < str.length(); i++) {
 			for(int j = 0; j < alpha.length; j++) {
 				if((int)str.charAt(i)==alpha[j]+65||(int)str.charAt(i)==alpha[j]+97) {
@@ -23,12 +22,29 @@ public class Num5 {
 				}
 			}
 		}
+		boolean bool = true;
 		int num = count[0];
 		for(int i = 1; i<alpha.length;i++) {
-			for(int j = i+1; j< alpha.length;j++) {
-				
+			if(num>count[i]) {
+				continue;
+			}else if(num<count[i] ) {
+				num=count[i];
+				bool=true;
+			}else{
+				bool = false;
 			}
-			System.out.println(i+ " : "+ count[i]);
 		}
+		for(int i = 0;i<alpha.length;i++) {
+			if(num==count[i] ) {
+				num=alpha[i];
+				break;
+			}
+		}
+		if(bool==false) {
+			System.out.println("?");
+		}else {		
+			System.out.println((char)(num+65));
+		}
+		
 	}	
 }
